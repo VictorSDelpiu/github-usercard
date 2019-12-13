1a834a5239a68c05782cc4199721b14e1bbb49cd
Victor Sanchez <v08311996@gmail.com>
2019-12-12 21:28:56 -0500

    Update

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,5 +1,31 @@
-/*
+
+  
+/* Step 1: using axios, send a GET request to the following URL 
+           (replacing the palceholder with your Github name):
+           https://api.github.com/users/<your name>
+*/
+
+/* Step 2: Inspect and study the data coming back, this is YOUR 
+   github info! You will need to understand the structure of this 
+   data in order to use it to build your component function 
+   Skip to Step 3.
+*/
+
+/* Step 4: Pass the data received from Github into your function, 
+           create a new component and add it to the DOM as a child of .cards
+*/
+
+/* Step 5: Now that you have your own card getting added to the DOM, either 
+          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
+          , manually find some other users' github handles, or use the list found 
+          at the bottom of the page. Get at least 5 different Github usernames and add them as
+          Individual strings to the friendsArray below.
+          
+          Using that array, iterate over it, requesting data for each user, creating a new card for each
+          user, and adding that card to the DOM.
+*/
 //Note: students can opt to use this array & iterate over it to display followers or they can access the url via axios
+/*
 const followersArray = [
   "tetondan",
   "dustinmyers",
@@ -17,6 +43,7 @@ axios
   .get("https://api.github.com/users/VictorSDelpiu")
   .then(res => {
     console.log(res);
+    
     mainCard.appendChild(GitHubCard(res.data));
   })
   .catch(err => console.log(err));
